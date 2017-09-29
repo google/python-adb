e8a6f8498898a8dd5edd42dee56866fe7236da0c
Fahrzin Hemmati <fahhem@users.noreply.github.com>
2017-09-29 12:14:15 -0700

    Better handle bytes/str in checksum in Python
    Should fix #64, #52, and #49 all in one!

--- adb/adb_protocol.py
+++ adb/adb_protocol.py
@@ -185,11 +185,17 @@ class AdbMessage(object):
   def CalculateChecksum(data):
     # The checksum is just a sum of all the bytes. I swear.
     if isinstance(data, bytearray):
-        total = sum(data)
+      total = sum(data)
     elif isinstance(data, bytes):
-        total = sum(map(ord, data.decode('ascii')))
-    else:
+      if data and isinstance(data[0], bytes):
+        # Python 2 bytes (str) index as single-character strings.
         total = sum(map(ord, data))
+      else:
+        # Python 3 bytes index as numbers (and PY2 empty strings sum() to 0)
+        total = sum(data)
+    else:
+      # Unicode strings (should never see?)
+      total = sum(map(ord, data))
     return total & 0xFFFFFFFF
 
   def Pack(self):
