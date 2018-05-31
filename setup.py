97e2aa9c3d29c5db4f190c57e2573e251d331465
Fahrzin Hemmati <fahhem@gmail.com>
2018-05-31 01:47:30 -0700

    Update to 1.3.0

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ except ImportError:
 setup(
     name = 'adb',
     packages = ['adb'],
-    version = '1.2.0',
+    version = '1.3.0',
     author = 'Fahrzin Hemmati',
     author_email = 'fahhem@gmail.com',
     maintainer = 'Fahrzin Hemmati',
